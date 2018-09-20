7e2e2c608ebf0183c334d7834441010b91cea8d5
we <we@2017070070-01.local>
2018-09-20 17:20:47 +0900

    init

---
+++ batch/crawler-batch.js
@@ -0,0 +1,28 @@
+'use strict';
+
+require('../lib/helper/bootstrap');
+
+const urlHelper = require('../lib/util/url-helper');
+
+const Crawler = require('../lib/crawler');
+
+const Batch = require('../lib/batch');
+const batch = new Batch('wonder-event-crawler');
+const crawler = new Crawler();
+
+const config = require('@wmp-sbd/config');
+
+const epService = require('../lib/ep');
+
+batch.booking('crawler', '00,20,40 * * * * *', async () => {
+  const sites = config.get('crawling.sites');
+
+  for(const site of sites) {
+    site.hostKey = urlHelper.getHostKeyFromUrl(site.uri);
+    site.ffuid = site.hostKey;
+    site.products = await crawler.run(site);
+
+    await epService.generateEp(site.products);
+    delete site.hostKey;
+  }
+});
