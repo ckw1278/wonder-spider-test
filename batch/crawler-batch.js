04b3bac388bac3f48f582b630d9294cbfcf74646
ckw1278 <ckw1278@naver.com>
2018-09-29 20:39:45 -0700

    e

--- batch/crawler-batch.js
+++ batch/crawler-batch.js
@@ -3,25 +3,17 @@
 require('../lib/helper/bootstrap');
 
 const urlHelper = require('../lib/util/url-helper');
-
-const Crawler = require('../lib/crawler');
-
 const Batch = require('../lib/batch');
-const batch = new Batch('wonder-event-crawler');
+const batch = new Batch('product-crawler');
+const Crawler = require('../lib/crawler');
 const crawler = new Crawler();
-
 const config = require('@wmp-sbd/config');
 
-const epService = require('../lib/ep');
+batch.booking('product-crawler', '* * * * * *', async () => {
+  const stores = config.get('crawling.stores');
 
-batch.booking('crawler', '20 26 * * * *', async () => {
-  const sites = config.get('crawling.sites');
-
-  for(const site of sites) {
-    site.hostKey = urlHelper.getHostKeyFromUrl(site.uri);
-    site.products = await crawler.run(site);
-    delete site.hostKey;
-
-    await epService.generateEp(site.products);
+  for(const store of stores) {
+    store.hostKey = urlHelper.getHostKeyFromUrl(store.uri);
+    await crawler.run(store);
   }
 });
