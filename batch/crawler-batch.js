f9ecf6a56bdbe829cb7642239b181a738f1f7d95
kwc <kwc@wemakeprice.com>
2018-10-02 22:43:11 +0900

    e

--- batch/crawler-batch.js
+++ batch/crawler-batch.js
@@ -1,19 +1,12 @@
-'use strict';
 
 require('../lib/helper/bootstrap');
 
-const urlHelper = require('../lib/util/url-helper');
 const Batch = require('../lib/batch');
 const batch = new Batch('product-crawler');
 const Crawler = require('../lib/crawler');
-const crawler = new Crawler();
-const config = require('@wmp-sbd/config');
 
-batch.booking('product-crawler', '* * * * * *', async () => {
-  const stores = config.get('crawling.stores');
+batch.booking('product-crawler', '00 00 * * * *', async () => {
+  const crawler = new Crawler();
 
-  for(const store of stores) {
-    store.hostKey = urlHelper.getHostKeyFromUrl(store.uri);
-    await crawler.run(store);
-  }
+  await crawler.run();
 });
